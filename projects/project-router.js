4e8d5b85ea6506feb16d32031023933a81d719b2
Barbara Moore <barbaraloismoore@gmail.com>
2020-07-17 16:55:43 -0400

    adding interview question responses

--- projects/project-router.js
+++ projects/project-router.js
@@ -19,19 +19,27 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  Projects.getProjectById(id)
-    .then((project) => {
-      if (project) {
-        res.json(project);
-      } else {
-        res
-          .status(404)
-          .json({ message: 'Could not find project with that ID.' });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ message: 'Failed to get project' });
-    });
+  Promise.all([
+    Projects.getProjectById(id),
+    Projects.getTasks(id),
+    Projects.getResources(id),
+  ]).then(([project, tasks, resources]) => {
+    res.status(200).json({ project, tasks, resources });
+  });
+
+  // Projects.getProjectById(id)
+  //   .then((project) => {
+  //     if (project) {
+  //       res.json(project);
+  //     } else {
+  //       res
+  //         .status(404)
+  //         .json({ message: 'Could not find project with that ID.' });
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).json({ message: 'Failed to get project' });
+  //   });
 });
 
 // GET resources by project ID
